541790bb3f37153ef65600d1e1fcecb7be07ba96
Filip Troníček <filip.tronicek@seznam.cz>
2023-02-26 16:32:50 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use] extern crate rocket;
 use rocket::serde::{Serialize, json::Json};
-use std::{time::{SystemTime, UNIX_EPOCH}};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
@@ -16,6 +16,20 @@ struct ResultWithDifference {
     diff_s: u64,
     unix_ms: u64,
     unix: u64
+}
+
+fn getUnixTimes() -> (u64, u64) {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    println!("{:?}", since_the_epoch);
+
+    let unix = since_the_epoch.as_secs();
+    let unix_ms = unix * 1000 +
+    since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    (unix_ms, unix)
+    return (unix_ms, unix);
 }
 
 #[derive(Serialize)]
@@ -34,15 +48,7 @@ struct ResponseWithDifference {
 
 #[get("/time")]
 fn time() -> Json<Response> {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    println!("{:?}", since_the_epoch);
-
-    let unix = since_the_epoch.as_secs();
-    let unix_ms = unix * 1000 +
-    since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    let (unix_ms, unix) = getUnixTimes();
     let result = ResultDefault { unix_ms, unix };
     let response = Response { status: "ok".to_string(), result };
     Json(response)
@@ -54,15 +60,7 @@ fn round_to_nearest(number: u64, denominator: u64) -> u64 {
 
 #[get("/time?<ts>")]
 fn time_query(ts: u64) -> Json<ResponseWithDifference> {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    println!("{:?}", since_the_epoch);
-
-    let unix = since_the_epoch.as_secs();
-    let unix_ms = unix * 1000 +
-    since_the_epoch.subsec_nanos() as u64 / 1_000_000;
+    let (unix_ms, unix) = getUnixTimes();
 
     let diff_ms = unix_ms - ts;
     let diff_s = round_to_nearest(diff_ms, 1000) / 1000;
